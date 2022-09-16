0bf10dce0e3d0e669afac0d9bb21cbea1db11492
SYSU-SP <113282882+SYSU-SP@users.noreply.github.com>
2022-09-16 15:35:47 +0800

    Add files via upload

---
+++ experiment/datasave/showresult.m
@@ -0,0 +1,35 @@
+clear;clc;close all;
+load('A_recovered.mat')
+load('P_recovered.mat')
+load('pupil_recovered.mat')
+load('intensity_recovered.mat')
+figure;
+const = 0.000;
+fsize =10;
+set(gcf,'Position',[100 100 400 400]);
+subplot(2,2,1)
+imshow(squeeze(A),[]);
+title('Amplitude','fontsize',fsize)
+subplot(2,2,2)
+imshow(squeeze(P),[]);
+title('Phase','fontsize',fsize)
+subplot(2,2,3)
+x = squeeze(angle(pupil));
+x(x<-0.9*pi) = x(x<-0.9*pi)+2*pi;
+imshow((squeeze(x)./pi),[-1,1],'colormap',hsv)
+title('Pupil','fontsize',fsize)
+subplot(2,2,4)
+imshow(intensity,[0,1],'colormap',gray)
+title('Intensity distribution','fontsize',fsize)
+
+
+
+
+
+
+
+
+
+
+
+
